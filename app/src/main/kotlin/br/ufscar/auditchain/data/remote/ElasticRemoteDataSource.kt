a9d5f12f29ade90043606e214c62c3823f8b6e74
Bruno Mendonça <bruno.mend94@gmail.com>
2020-04-10 15:26:15 -0300

    Created module app

--- app/src/main/kotlin/br/ufscar/auditchain/data/remote/ElasticRemoteDataSource.kt
+++ app/src/main/kotlin/br/ufscar/auditchain/data/remote/ElasticRemoteDataSource.kt
@@ -1,7 +1,6 @@
 package br.ufscar.auditchain.data.remote
 
 import io.reactivex.rxjava3.core.Single
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
